cf38cb99ead21e6ae7bb8929cc36fde402653350
amirsepehr2020 <sepehr2sodoury@gmail.com>
2026-01-30 14:12:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ textarea#textInput{flex:1; resize:none; border:none; outline:none; background:tr
  <button class="send-btn">➤</button>  
 </form>  
 </div>  <div class="version">  
-  Realse 1.0
+  Releases 1.0
   <div class="creator">  
     <span>by Red Boy</span>  
     <img src="https://s6.uupload.ir/files/inshot_20251227_204536460_j5j8.png">  
