978e989acffa3c49511d7e6e019e35c688822225
amirsepehr2020 <sepehr2sodoury@gmail.com>
2026-01-30 09:10:06 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ client = Groq(
 def ask_ai(user_message):
     try:
         completion = client.chat.completions.create(
-            model="llama3-8b-8192",
+            model="llama-3.1-8b-instant",
             messages=[
                 {"role": "user", "content": user_message}
             ],
