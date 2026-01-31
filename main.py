4a382d7d9b077ddf9a40b354b6b0697e86513e82
amirsepehr2020 <sepehr2sodoury@gmail.com>
2026-02-01 00:24:15 +0330

    Update release version from 1.0 to 1.8

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ textarea#textInput{flex:1; resize:none; border:none; outline:none; background:tr
  <button class="send-btn">➤</button>  
 </form>  
 </div>  <div class="version">  
-  Releases 1.0
+  Release 1.8
   <div class="creator">  
     <span>by Red Boy</span>  
     <img src="https://s6.uupload.ir/files/inshot_20251227_204536460_j5j8.png">  
